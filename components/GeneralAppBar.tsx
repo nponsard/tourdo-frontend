49b9192c9c83debe1f307367963e364d1b31191c
Nils Ponsard <nilsponsard@gmail.com>
2022-02-01 11:27:23 +0100

    refactor app bar

---
+++ components/GeneralAppBar.tsx
@@ -0,0 +1,146 @@
+import AddIcon from "@mui/icons-material/Add";
+import ArrowDropDownIcon from "@mui/icons-material/ArrowDropDown";
+import { AppBar, Button, IconButton, Menu, MenuItem, Toolbar, Typography } from "@mui/material";
+import Link from "next/link";
+import router from "next/router";
+import { useContext, useState } from "react";
+import { ClearLocalStorage, FetchLogout, LoginContext } from "../utils/auth";
+
+export default function GeneralAppBar() {
+    const { user, tokenPair, setTokenPair } = useContext(LoginContext);
+
+    const [addMenuAnchor, setAddMenuAnchor] = useState<HTMLElement | null>(null);
+    const [userMenuAnchor, setUserMenuAnchor] = useState<HTMLElement | null>(null);
+
+    const handleUserMenu = (event: React.MouseEvent<HTMLElement>) => {
+        setUserMenuAnchor(event.currentTarget);
+    };
+    const handleUserMenuClose = () => {
+        setUserMenuAnchor(null);
+    };
+
+    const handleLogout = () => {
+        if (tokenPair) {
+            setUserMenuAnchor(null);
+            FetchLogout(tokenPair, setTokenPair)
+                .then(() => {
+                    ClearLocalStorage();
+                    setTokenPair(null);
+                })
+                .catch((err) => {
+                    console.log(err);
+                });
+        }
+    };
+
+    return (
+        <AppBar position="sticky">
+            <Toolbar>
+                <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+                    <Link href={"/"}>TourDO</Link>
+                </Typography>
+
+                {user === undefined && <div>loading</div>}
+
+                {tokenPair && user ? (
+                    <>
+                        <IconButton
+                            color="inherit"
+                            size="large"
+                            onClick={(e) => {
+                                setAddMenuAnchor(e.currentTarget);
+                            }}
+                        >
+                            <AddIcon />
+                        </IconButton>
+
+                        <Menu
+                            id="user-menu"
+                            anchorEl={addMenuAnchor}
+                            anchorOrigin={{
+                                vertical: "top",
+                                horizontal: "right",
+                            }}
+                            keepMounted
+                            transformOrigin={{
+                                vertical: "top",
+                                horizontal: "right",
+                            }}
+                            open={Boolean(addMenuAnchor)}
+                            onClose={() => {
+                                setAddMenuAnchor(null);
+                            }}
+                        >
+                            <MenuItem
+                                onClick={() => {
+                                    setAddMenuAnchor(null);
+                                    router.push(`/create/tournament`);
+                                }}
+                            >
+                                Tournament
+                            </MenuItem>
+
+                            <MenuItem
+                                onClick={() => {
+                                    setAddMenuAnchor(null);
+                                    router.push(`/create/team`);
+                                }}
+                            >
+                                Team
+                            </MenuItem>
+                        </Menu>
+
+                        <Button
+                            aria-label="account of current user"
+                            aria-controls="menu-appbar"
+                            aria-haspopup="true"
+                            onClick={handleUserMenu}
+                            color="inherit"
+                            sx={{ textTransform: "none" }}
+                            endIcon={<ArrowDropDownIcon />}
+                        >
+                            {user.username} {user.admin && "(admin)"}
+                        </Button>
+                        <Menu
+                            id="user-menu"
+                            anchorEl={userMenuAnchor}
+                            anchorOrigin={{
+                                vertical: "top",
+                                horizontal: "right",
+                            }}
+                            keepMounted
+                            transformOrigin={{
+                                vertical: "top",
+                                horizontal: "right",
+                            }}
+                            open={Boolean(userMenuAnchor)}
+                            onClose={handleUserMenuClose}
+                        >
+                            <MenuItem
+                                onClick={() => {
+                                    setUserMenuAnchor(null);
+                                    router.push(`/users/${user.id}`);
+                                }}
+                            >
+                                Profile
+                            </MenuItem>
+                            <MenuItem
+                                onClick={() => {
+                                    setUserMenuAnchor(null);
+                                    router.push(`/password`);
+                                }}
+                            >
+                                Change password
+                            </MenuItem>
+                            <MenuItem onClick={handleLogout}>Logout</MenuItem>
+                        </Menu>
+                    </>
+                ) : (
+                    <Link href="/login" passHref>
+                        <Button color="inherit">Login</Button>
+                    </Link>
+                )}
+            </Toolbar>
+        </AppBar>
+    );
+}
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -12,6 +12,7 @@ import Head from "next/head";
 import Link from "next/link";
 import { useRouter } from "next/router";
 import { useEffect, useState } from "react";
+import GeneralAppBar from "../components/GeneralAppBar";
 import "../styles/globals.css";
 import {
     FetchLogout,
@@ -26,7 +27,6 @@ import { FetchCurrentUser, User } from "../utils/users";
 function MyApp({ Component, pageProps }: AppProps) {
     // undefined : not yet loaded
     // null : not logged in
-
     const [user, setUser] = useState<User | undefined | null>(undefined);
     const [tokenPair, _setTokenPair] = useState<TokenPair | undefined | null>(undefined);
 
@@ -35,8 +35,6 @@ function MyApp({ Component, pageProps }: AppProps) {
 
         _setTokenPair(value);
     };
-
-    const router = useRouter();
 
     // charge les tokens au lancement
 
@@ -67,30 +65,6 @@ function MyApp({ Component, pageProps }: AppProps) {
         }
     }, [tokenPair]);
 
-    const [userMenuAnchor, setUserMenuAnchor] = useState<null | HTMLElement>(null);
-
-    const [addMenuAnchor, setAddMenuAnchor] = useState<null | HTMLElement>(null);
-    const handleUserMenu = (event: React.MouseEvent<HTMLElement>) => {
-        setUserMenuAnchor(event.currentTarget);
-    };
-    const handleUserMenuClose = () => {
-        setUserMenuAnchor(null);
-    };
-
-    const handleLogout = () => {
-        if (tokenPair) {
-            setUserMenuAnchor(null);
-            FetchLogout(tokenPair, setTokenPair)
-                .then(() => {
-                    ClearLocalStorage();
-                    setTokenPair(null);
-                })
-                .catch((err) => {
-                    console.log(err);
-                });
-        }
-    };
-
     return (
         <LoginContext.Provider
             value={{
@@ -100,124 +74,13 @@ function MyApp({ Component, pageProps }: AppProps) {
                 setTokenPair: (newTokenPair: TokenPair | undefined | null) => setTokenPair(newTokenPair),
             }}
         >
-            <div>
-                <Head>
-                    <title>TOURDO</title>
-                    <meta name="description" content="DO tournament manager" />
-                    <link rel="icon" href="/favicon.ico" />
-                </Head>
+            <Head>
+                <title>TOURDO</title>
+                <meta name="description" content="DO tournament manager" />
+                <link rel="icon" href="/favicon.ico" />
+            </Head>
 
-                <Box sx={{ flexGrow: 1 }}>
-                    <AppBar position="sticky">
-                        <Toolbar>
-                            <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-                                <Link href={"/"}>TourDO</Link>
-                            </Typography>
-
-                            {user === undefined && <div>loading</div>}
-
-                            {tokenPair && user ? (
-                                <>
-                                    <IconButton
-                                        color="inherit"
-                                        size="large"
-                                        onClick={(e) => {
-                                            setAddMenuAnchor(e.currentTarget);
-                                        }}
-                                    >
-                                        <AddIcon />
-                                    </IconButton>
-
-                                    <Menu
-                                        id="user-menu"
-                                        anchorEl={addMenuAnchor}
-                                        anchorOrigin={{
-                                            vertical: "top",
-                                            horizontal: "right",
-                                        }}
-                                        keepMounted
-                                        transformOrigin={{
-                                            vertical: "top",
-                                            horizontal: "right",
-                                        }}
-                                        open={Boolean(addMenuAnchor)}
-                                        onClose={() => {
-                                            setAddMenuAnchor(null);
-                                        }}
-                                    >
-                                        <MenuItem
-                                            onClick={() => {
-                                                setAddMenuAnchor(null);
-                                                router.push(`/create/tournament`);
-                                            }}
-                                        >
-                                            Tournament
-                                        </MenuItem>
-
-                                        <MenuItem
-                                            onClick={() => {
-                                                setAddMenuAnchor(null);
-                                                router.push(`/create/team`);
-                                            }}
-                                        >
-                                            Team
-                                        </MenuItem>
-                                    </Menu>
-
-                                    <Button
-                                        aria-label="account of current user"
-                                        aria-controls="menu-appbar"
-                                        aria-haspopup="true"
-                                        onClick={handleUserMenu}
-                                        color="inherit"
-                                        sx={{ textTransform: "none" }}
-                                        endIcon={<ArrowDropDownIcon />}
-                                    >
-                                        {user.username} {user.admin && "(admin)"}
-                                    </Button>
-                                    <Menu
-                                        id="user-menu"
-                                        anchorEl={userMenuAnchor}
-                                        anchorOrigin={{
-                                            vertical: "top",
-                                            horizontal: "right",
-                                        }}
-                                        keepMounted
-                                        transformOrigin={{
-                                            vertical: "top",
-                                            horizontal: "right",
-                                        }}
-                                        open={Boolean(userMenuAnchor)}
-                                        onClose={handleUserMenuClose}
-                                    >
-                                        <MenuItem
-                                            onClick={() => {
-                                                setUserMenuAnchor(null);
-                                                router.push(`/users/${user.id}`);
-                                            }}
-                                        >
-                                            Profile
-                                        </MenuItem>
-                                        <MenuItem
-                                            onClick={() => {
-                                                setUserMenuAnchor(null);
-                                                router.push(`/password`);
-                                            }}
-                                        >
-                                            Change password
-                                        </MenuItem>
-                                        <MenuItem onClick={handleLogout}>Logout</MenuItem>
-                                    </Menu>
-                                </>
-                            ) : (
-                                <Link href="/login" passHref>
-                                    <Button color="inherit">Login</Button>
-                                </Link>
-                            )}
-                        </Toolbar>
-                    </AppBar>
-                </Box>
-            </div>
+            <GeneralAppBar />
 
             <Box
                 sx={{
